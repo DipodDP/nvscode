c8ea7a74ff3788093a81def0226696804b8ea163
dp <dipoddp@gmail.com>
2025-09-26 15:59:51 +0700

    remove copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,14 +1,14 @@
-local is_online = require("config.utils").is_online
+-- local is_online = require("config.utils").is_online
 
 return {
   "saghen/blink.cmp",
   build = 'cargo build --release',
-  dependencies = {
-    {
-      "giuxtaposition/blink-cmp-copilot",
-      enabled = is_online,
-    },
-  },
+  -- dependencies = {
+  --   {
+  --     "giuxtaposition/blink-cmp-copilot",
+  --     enabled = is_online,
+  --   },
+  -- },
   opts = function(_, opts)
     opts.enabled = function()
       local filetype = vim.bo[0].filetype
@@ -32,24 +32,24 @@ return {
       use_proximity = true,
     })
     opts.sources = vim.tbl_deep_extend("force", opts.sources or {}, {
-      default = function(_)
-        local success, node = pcall(vim.treesitter.get_node)
-        if vim.bo.filetype == "lua" then
-          local src = { "lazydev", "lsp", "path" }
-          if is_online() then
-            table.insert(src, "copilot")
-          end
-          return src
-        elseif success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
-          return { "buffer" }
-        else
-          local src = { "lsp", "path", "snippets", "buffer" }
-          if is_online() then
-            table.insert(src, "copilot")
-          end
-          return src
-        end
-      end,
+      -- default = function(_)
+      --   local success, node = pcall(vim.treesitter.get_node)
+      --   if vim.bo.filetype == "lua" then
+      --     local src = { "lazydev", "lsp", "path" }
+      --     if is_online() then
+      --       table.insert(src, "copilot")
+      --     end
+      --     return src
+      --   elseif success and node and vim.tbl_contains({ "comment", "line_comment", "block_comment" }, node:type()) then
+      --     return { "buffer" }
+      --   else
+      --     local src = { "lsp", "path", "snippets", "buffer" }
+      --     if is_online() then
+      --       table.insert(src, "copilot")
+      --     end
+      --     return src
+      --   end
+      -- end,
       providers = {
         lsp = {
           name = "lsp",
@@ -87,15 +87,15 @@ return {
           min_keyword_length = 4,
           score_offset = 15,
         },
-        copilot = {
-          name = "copilot",
-          enabled = is_online(),
-          module = "blink-cmp-copilot",
-          kind = "Copilot",
-          min_keyword_length = 5,
-          score_offset = 10,
-          async = true,
-        },
+        -- copilot = {
+        --   name = "copilot",
+        --   enabled = is_online(),
+        --   module = "blink-cmp-copilot",
+        --   kind = "Copilot",
+        --   min_keyword_length = 5,
+        --   score_offset = 10,
+        --   async = true,
+        -- },
       },
     })
 
