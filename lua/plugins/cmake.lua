8ccd1a3e04781c900c44c7fa43963efe4cf5e3e9
dp <dipoddp@gmail.com>
2025-12-15 22:12:10 +0700

    fix: errors in config

--- lua/plugins/cmake.lua
+++ lua/plugins/cmake.lua
@@ -1,5 +1,12 @@
 return {
-  "Civitasv/cmake-tools.nvim",
-  opts = {},
-  ft = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
+  {
+    "Civitasv/cmake-tools.nvim",
+    opts = {},
+    ft = { "c", "cpp", "objc", "objcpp", "h", "hpp" },
+  },
+  {
+    "p00f/clangd_extensions.nvim",
+    ft = { "c", "cpp", "objc", "objcpp", "cuda", "proto" },
+    opts = {},
+  },
 }
